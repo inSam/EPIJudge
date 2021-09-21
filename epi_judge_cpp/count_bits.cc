07129df0541bf8071e7c26add45aa2cde4ccd2e0
Sam Xu <samx@stanford.edu>
2021-09-20 22:26:12 -0700

    'test commit from visual studio'

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -1,13 +1,17 @@
 #include "test_framework/generic_test.h"
 
 short CountBits(unsigned int x) {
-  // TODO - you fill in here.
-  return 0;
+    short ctr = 0;
+    while (x > 0) {
+        x &= x - 1;
+        ctr++;
+    }
+    return ctr;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "count_bits.cc", "count_bits.tsv", &CountBits,
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x"};
+    return GenericTestMain(args, "count_bits.cc", "count_bits.tsv", &CountBits,
                          DefaultComparator{}, param_names);
 }
