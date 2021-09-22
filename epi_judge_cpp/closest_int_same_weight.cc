6309450edff819000ed13b4a192028a2c7d487ba
Sam Xu <samx@stanford.edu>
2021-09-22 10:37:41 -0700

    same weight and primitive mult

--- epi_judge_cpp/closest_int_same_weight.cc
+++ epi_judge_cpp/closest_int_same_weight.cc
@@ -1,7 +1,18 @@
 #include "test_framework/generic_test.h"
 unsigned long long ClosestIntSameBitCount(unsigned long long x) {
-  
-  return 0;
+    if (x & 1) {
+        unsigned long long x_mask = (~x) & ~(~x - 1); //select the lsb zero bit
+        x ^= x_mask;
+        x_mask = x_mask >> 1; //select the immediately preceding 1 bit
+        x ^= x_mask;
+    }
+    else {
+        unsigned long long x_mask = x & ~(x - 1); // Selects the lsb one bit
+        x_mask = x_mask >> 1; // selects the immediately preceeding 0 bit 
+        x &= (x - 1); // flip the lsb one bit
+        x ^= x_mask; // flip the precedding one bit
+    }
+    return x;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/primitive_multiply.cc
+++ epi_judge_cpp/primitive_multiply.cc
@@ -1,9 +1,28 @@
 #include "test_framework/generic_test.h"
-unsigned long long Multiply(unsigned long long x, unsigned long long y) {
-  // TODO - you fill in here.
-  return 0;
+unsigned long long Add(unsigned long long x, unsigned long long y) {
+    unsigned long long sum = 0, carry = 0, tmp_x = x, tmp_y = y, k = 1;
+
+    while (tmp_x || tmp_y) {
+        unsigned long long bit_x = x & k, bit_y = y & k;
+        unsigned long long carry_out = (bit_x & bit_y) | (bit_x & carry) | (bit_y & carry);
+        sum |= (bit_x ^ bit_y ^ carry);
+        carry = carry_out << 1, k <<= 1, tmp_x >>= 1, tmp_y >>= 1;
+    }
+    return sum | carry;
 }
 
+unsigned long long Multiply(unsigned long long x, unsigned long long y) {
+    unsigned long long prod = 0;
+    while (y) {
+        if (y & 1) {
+            prod = Add(prod, x);
+        }
+        x <<= 1; y >>= 1;
+    }
+    return prod;
+}
+
+ 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "y"};
